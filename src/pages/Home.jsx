d2f4f7ab48e8150873dd2f17eb5c06679bfb4c5f
Kristinblun <kristinblun@gmail.com>
2025-10-15 22:23:26 -0700

    add home page and asset images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import beachFeature from '../assets/beach-unsplash.jpg';
+import hotel from '../assets/hotel.jpg';
+import plane from '../assets/plane.jpg';
+import cruise from '../assets/cruise.jpg';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className=" p-4">
+      <div className="relative">
+        <img src={beachFeature} className="w-full h-128" />
+        <div className="absolute inset-0 bg-black/20 flex items-center justify-center">
+          <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold text-center px-4">
+            The Taniti people invite you to a tropical paradise
+          </h1>
+        </div>
+      </div>
+
+      <div className="bg-yellow-50 py-10 text-center mt-4">
+        <div className="text-2xl font-medium text-gray-800 max-w-3xl mx-auto leading-relaxed space-y-6">
+          <p>
+            Relax on golden warm beaches and snorkel in crystal-clear waters.
+          </p>
+          <p>
+            Hike a volcano, zipline through lush rainforests — fun for the whole
+            family!
+          </p>
+        </div>
+      </div>
+
+      <Link to="/booking">
+        <div className=" relative mt-4 flex h-60 rounded-xl shadow-md">
+          <h2 className="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold bg-black/40">
+            {' '}
+            Book your trip now!
+          </h2>
+          <img src={cruise} className="w-1/3" />
+          <img src={plane} className="w-1/3" />
+          <img src={hotel} className="w-1/3" />
+        </div>
+      </Link>
+    </div>
+  );
 }
