c6f2ad96a834fa18a182b4fe4c9b873629284ae6
Kristinblun <kristinblun@gmail.com>
2025-10-21 20:23:31 -0700

    Add footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-yellow-50 text-cyan-700 py-6 mt-10 text-center border-t border-cyan-200/40">
+      <p className="text-lg italic tracking-wide mb-2">
+        🌺 May the spirit of Taniti embrace you. We will see you soon. 🌺
+      </p>
+      <p className="text-sm">
+        {' '}
+        © {new Date().getFullYear()} Taniti Adventures. All rights reserved{' '}
+      </p>
+    </footer>
+  );
 }
